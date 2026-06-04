8e6c7752ed9fc94f64d5c3f36879d40d7ad90493
Rolf Leo <rolf.leo.stumpf@gmail.com>
2026-06-04 20:05:30 +0200

    Fjern bilde nr. 5 fra produktvisning (indeks 6)

--- app/produkt/[id]/page.tsx
+++ app/produkt/[id]/page.tsx
@@ -40,7 +40,7 @@ function sorterVarianter(varianter: any[]) {
 const SKJUL_VARIANTER: Record<string, string[]> = {};
 
 const BEHOLD_BILDER: Record<string, number[]> = {
-  '1767124394830204928': [0, 3, 4, 5, 6, 8, 12, 18, 20, 21, 23, 27, 28, 29, 35, 36, 37],
+  '1767124394830204928': [0, 3, 4, 5, 8, 12, 18, 20, 21, 23, 27, 28, 29, 35, 36, 37],
 };
 
 const TILLATTE_VIDER: Record<string, string[]> = {
